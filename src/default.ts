19fc71fce23aa585c38866e6c7ea1b2b8dfff875
cloudzun <info@cloudzun.com>
2023-03-10 08:06:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/default.ts
@@ -0,0 +1,12 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: true,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 本网站基于 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 技术并运行在 [Azure](http://www.azure.com/) 。
+- 由 [@CloudZun](https://github.com/cloudzun) 运维， [项目代码](https://github.com/cloudzun/chatgpt-vercel) 源自 [@ourongxing](https://github.com/ourongxing) 和 [@ddiu8081](https://github.com/ddiu8081/chatgpt-demo)。
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
