bc55dea56b57e275a884a5ef44ca91e73db1065b
CloudZun <info@cloudzun.com>
2023-03-10 16:17:26 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -7,6 +7,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本网站基于 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 技术并运行在 [Azure](http://www.azure.com/) 。
+- 本网站基于 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 技术，仅用于演示。
 - 由 [@CloudZun](https://github.com/cloudzun) 运维， [项目代码](https://github.com/cloudzun/chatgpt-vercel) 源自 [@ourongxing](https://github.com/ourongxing) 和 [@ddiu8081](https://github.com/ddiu8081/chatgpt-demo)。
 - <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
