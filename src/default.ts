43e1604fff69638b06326bdb6a0a01973b260f77
CloudZun <info@cloudzun.com>
2023-03-15 08:03:12 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,5 +8,5 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本网站基于 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 技术，仅用于演示。
-- 由 [@CloudZun](https://github.com/cloudzun) 运维， [项目代码](https://github.com/cloudzun/chatgpt-vercel) 源自 [@ourongxing](https://github.com/ourongxing) 和 [@ddiu8081](https://github.com/ddiu8081/chatgpt-demo)。
+- 由 [CloudZun](https://github.com/cloudzun) 运维， [项目代码](https://github.com/cloudzun/chatgpt-vercel) 源自 [ourongxing](https://github.com/ourongxing) 和 [ddiu8081](https://github.com/ddiu8081/chatgpt-demo)。
 - 输入框中输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索内置角色。<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。`
