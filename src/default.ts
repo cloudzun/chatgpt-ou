2dcae5854ef989cabe5c60fb274c6de26e1e8fe6
CloudZun <info@cloudzun.com>
2023-03-20 17:48:12 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -11,4 +11,4 @@ export const defaultMessage = `
 - 输入框中输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索内置角色。<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。
 - 产品及服务报价：
 -   OpenAI 个人版 Key：19元/个 ；包月：69元/月； 包年：599元/年
--   PaaS部署：99元/次； 平台容器化部署：199元/次； 管理系统：3999元/套 `
+-   PaaS 部署：99元/次； Docker 部署：199元/次； 管理系统：3999元/套 `
