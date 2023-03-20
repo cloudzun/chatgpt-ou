772ecc386eac5047b2cfa06102991ec9f43cfac9
CloudZun <info@cloudzun.com>
2023-03-20 17:47:18 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -10,5 +10,5 @@ export const defaultMessage = `
 - 本网站基于 [OpenAI API](https://platform.openai.com/docs/guides/chat) 技术。
 - 输入框中输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索内置角色。<kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。
 - 产品及服务报价：
--   个人密钥：19元/个 ；包月：69元/月； 包年：599元/年
--   平台容器化部署：199元/次； PaaS部署： 99元/次 ； 管理系统：3999元/套 `
+-   OpenAI 个人版 Key：19元/个 ；包月：69元/月； 包年：599元/年
+-   PaaS部署：99元/次； 平台容器化部署：199元/次； 管理系统：3999元/套 `
